2f21ddf48b2e72e2a597d20df6280bed541ea9b5
capGoblin <dharshan2457@gmail.com>
2023-12-25 23:06:19 +0530

    web scraper: ebay script

--- src/index.ts
+++ src/index.ts
@@ -79,6 +79,52 @@ import puppeteer from "puppeteer";
 
   // ... Perform scraping logic for Amazon
 
+  // Open a new tab for eBay
+  const ebayPage = await browser.newPage();
+  await ebayPage.goto("https://www.ebay.com");
+
+  // Wait for the search input field to load and become visible
+  await ebayPage.waitForSelector("#gh-ac");
+
+  // Type your search query into the search bar
+  await ebayPage.type("#gh-ac", "Redmi 13C");
+
+  // Simulate pressing the Enter key to submit the search
+  await ebayPage.keyboard.press("Enter");
+
+  // Wait for the search results to load
+  await ebayPage.waitForSelector(".s-item");
+
+  // Extract information for the first three products
+  const EbayproductInfo = await ebayPage.evaluate(() => {
+    const products = Array.from(document.querySelectorAll(".s-item")).slice(
+      1,
+      11
+    );
+
+    return products.map((product) => {
+      const titleElement = product.querySelector(".s-item__title");
+      const title = titleElement?.textContent?.trim() ?? "No title";
+
+      const imageElement = product.querySelector(".s-item__image img");
+      const imageSrc = imageElement
+        ? imageElement.getAttribute("src")
+        : "No image";
+
+      const priceElement = product.querySelector(".s-item__price");
+      const price =
+        priceElement && priceElement.textContent
+          ? priceElement.textContent.trim()
+          : "No price";
+
+      return { title, imageSrc, price };
+    });
+  });
+
+  // console.log(productInfo);
+  // Display collected information in console
+  console.log(EbayproductInfo); // Display collected information in console
+
   // Close the browser
   // await browser.close();
 })();
