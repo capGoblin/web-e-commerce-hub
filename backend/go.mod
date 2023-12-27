7412d222d2cc592075ea1cede3056b97439062de
capGoblin <dharshan2457@gmail.com>
2023-12-28 01:09:17 +0530

    go runs web scraper script

--- backend/go.mod
+++ backend/go.mod
@@ -3,3 +3,5 @@ module web-ecommerce
 go 1.21.5
 
 require github.com/rs/cors v1.10.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,3 +1,67 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+
+// 	"net/http"
+// 	"os/exec"
+
+// 	"github.com/rs/cors"
+// )
+
+// // func main() {
+// //     cmd := exec.Command("npx", "ts-node", "./script/index.ts")
+// //     out, err := cmd.Output()
+
+// //     if err != nil {
+// //         log.Fatal(err)
+// //     }
+
+// //     fmt.Println(string(out))
+// // }
+
+// func corsMiddleware(handler http.Handler) http.Handler {
+//     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//         w.Header().Set("Access-Control-Allow-Origin", "*")
+//         w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+//         w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+//         // If this is an OPTIONS request (a preflight request), respond with OK.
+//         if r.Method == "OPTIONS" {
+//             w.WriteHeader(http.StatusOK)
+//             return
+//         }
+
+//         handler.ServeHTTP(w, r)
+//     })
+// }
+
+// func main() {
+//     mux := http.NewServeMux()
+
+// 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//         cmd := exec.Command("npx", "ts-node", "./script/index.ts")
+// 		out, err := cmd.Output()
+
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+
+// 		fmt.Fprint(w, string(out))
+// 	})
+// 	// Create a CORS handler with allowed origins (replace "*" with your allowed origin)
+// 	corsHandler := cors.New(cors.Options{
+// 		AllowedOrigins:   []string{"*"},     // Set your allowed origins here
+// 		AllowedMethods:   []string{"GET"},   // Adjust the allowed HTTP methods
+// 		AllowedHeaders:   []string{"*"},     // Set allowed headers
+// 		AllowCredentials: true,              // Allow credentials such as cookies
+// 	}).Handler(mux)
+
+// 	// Start the server with CORS handling
+// 	log.Fatal(http.ListenAndServe(":8080", corsHandler))
+// }
+
 package main
 
 import (
@@ -6,23 +70,26 @@ import (
 	"net/http"
 	"os/exec"
 
+	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
 
 func main() {
-	mux := http.NewServeMux()
-
+    mux := mux.NewRouter()
     mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		cmd := exec.Command("node", "./script/index.ts")
-		out, err := cmd.Output()
+        cmd := exec.Command("npx", "ts-node", "./script/index.ts")
+        out, err := cmd.Output()
 
-		if err != nil {
-			log.Fatal(err)
-		}
+        if err != nil {
+            log.Fatal(err)
+        }
 
-		fmt.Fprint(w, string(out))
+        fmt.Fprint(w, string(out))
     })
-	handler := cors.Default().Handler(mux)
 
-    http.ListenAndServe(":8080", handler)
+    // Wrap the router with the cors middleware
+    handler := cors.Default().Handler(mux)
+
+    // Start the server with the cors middleware
+    log.Fatal(http.ListenAndServe(":8080", handler))
 }
