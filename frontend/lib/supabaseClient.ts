5af131adff0530ad93ece270a754bb60d03de6fc
capGoblin <dharshan2457@gmail.com>
2024-02-19 13:47:29 +0530

    Update supabaseClient.ts

--- frontend/lib/supabaseClient.ts
+++ frontend/lib/supabaseClient.ts
@@ -1,11 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+require("dotenv").config();
 
-const supabaseUrl = process.env.NEXT_APP_URL;
-const supabaseKey = process.env.NEXT_APP_ANON_KEY;
+const supabaseUrl = process.env.NEXT_APP_URL!;
+const supabaseKey = process.env.NEXT_APP_ANON_KEY!;
 // const supabase = createClient(supabaseUrl!, supabaseKey!);
 console.log(supabaseUrl);
 console.log(supabaseKey);
-export const supabase = createClient(
-  "https://swkihtahvppxvpwsqmhh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3a2lodGFodnBweHZwd3NxbWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc2NjUyNTIsImV4cCI6MjAyMzI0MTI1Mn0.ikDx_NVQRHGql_z-rVGqIQRAxMzhww4xihEEQFucBbM"
-);
+export const supabase = createClient(supabaseUrl, supabaseKey);
