5bcb8fe36de2ca4223eaea523abfc11d7e072898
capGoblin <dharshan2457@gmail.com>
2024-02-23 23:18:05 +0530

    Update Supabase environment variables

--- frontend/lib/supabaseClient.ts
+++ frontend/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 require("dotenv").config();
 
-const supabaseUrl = process.env.NEXT_APP_URL!;
-const supabaseKey = process.env.NEXT_APP_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_APP_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_APP_ANON_KEY!;
 // const supabase = createClient(supabaseUrl!, supabaseKey!);
 console.log(supabaseUrl);
 console.log(supabaseKey);
